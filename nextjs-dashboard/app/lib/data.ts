53c3bd1b94e8919046c760f1008b20b4bae63f05
ShresthaSamita <samita.shrestha@jivasoft.com>
2024-09-09 10:40:20 -0500

    feat: use `suspense` for other components as well

--- nextjs-dashboard/app/lib/data.ts
+++ nextjs-dashboard/app/lib/data.ts
@@ -31,6 +31,7 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
+    // await new Promise((resolve) => setTimeout(resolve, 3000));
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
@@ -41,6 +42,7 @@ export async function fetchLatestInvoices() {
     const latestInvoices = data.rows.map((invoice) => ({
       ...invoice,
       amount: formatCurrency(invoice.amount),
+      
     }));
     return latestInvoices;
   } catch (error) {
